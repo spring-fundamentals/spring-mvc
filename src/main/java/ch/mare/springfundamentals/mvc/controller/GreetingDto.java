6dd4a998c9e71eb488c4931fffe4d242b327cc69
Marius Reusch <marius.reusch@gmail.com>
2019-01-12 10:28:16 +0100

    add configuration for dates

--- src/main/java/ch/mare/springfundamentals/mvc/controller/GreetingDto.java
+++ src/main/java/ch/mare/springfundamentals/mvc/controller/GreetingDto.java
@@ -1,20 +1,28 @@
 package ch.mare.springfundamentals.mvc.controller;
 
+import java.time.LocalDate;
+
 public class GreetingDto {
 
-    private String greeting;
-    private String name;
+  private String greeting;
+  private String name;
+  private LocalDate greetingDate;
 
-    public GreetingDto(String greeting, String name) {
-        this.greeting = greeting;
-        this.name = name;
-    }
+  public GreetingDto(String greeting, String name) {
+    this.greeting = greeting;
+    this.name = name;
+    this.greetingDate = LocalDate.now();
+  }
 
-    public String getGreeting() {
-        return greeting;
-    }
+  public String getGreeting() {
+    return greeting;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
+
+  public LocalDate getGreetingDate() {
+    return greetingDate;
+  }
 }
